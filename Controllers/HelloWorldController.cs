8b3d4054006eafe9822a0412c32ee0b335ac381a
LLLJT <48343971+LLLJT@users.noreply.github.com>
2022-06-17 11:45:01 +0800

    test_file

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "default action";
+        }
+
+        public string hello(string name,int age) {
+            name = "abc";
+            age = 9;
+            return HtmlEncoder.Default.Encode($"Hello{name} age {age}");
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDemo.Models
+{
+    public class Movie
+    {
+        /*
+         * add validation
+         * [StringLength(60,MinimumLength=3)]字段长度
+         * [Required]非空
+         * decimal int float DateTime[Required]
+         * [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+         */
+        public int Id { get; set; }
+        public string Title { get; set; }
+        //日期类型
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        public string Rating { get; set; }
+    }
+}
